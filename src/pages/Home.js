19de9a881f00f645e358db62654a5a4aea3bc95d
Akindeji <akindejioluwaseun015@gmail.com>
2021-08-28 17:06:37 +0100

    Added hashrouter

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import { TodoContextWrapper } from "../context/TodoContext/index";
+import TodoList from "../components/TodoList";
+import AddLayer from "../components/AddLayer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <TodoContextWrapper>
+      <div className="App">
+        <h1>React Todo App</h1>
+        <div className="todo">
+          <AddLayer />
+          <main className="todo__main">
+            <TodoList />
+          </main>
+        </div>
+      </div>
+    </TodoContextWrapper>
+  );
+}
+
+export default App;
